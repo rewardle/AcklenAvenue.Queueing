512e9d01b0d39a8c8a629b21cc2d431fc167d701
Byron Sommardahl <byron@sommardahl.com>
2015-06-11 22:47:08 -0600

    Update AssemblyInfo.cs

--- src/AcklenAvenue.Queueing/Properties/AssemblyInfo.cs
+++ src/AcklenAvenue.Queueing/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AcklenAvenue.Queuing")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Lightweight library for queueing.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AcklenAvenue.Queuing")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Acklen Avenue 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
